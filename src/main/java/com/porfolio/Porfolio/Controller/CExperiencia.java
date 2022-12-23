465f47e4677d6b1ab13ffcc9a6cf6b3a09018772
Ricardo G. Alvarez <59462673+gipy09@users.noreply.github.com>
2022-12-23 14:39:57 -0300

    Update CExperiencia.java

--- src/main/java/com/porfolio/Porfolio/Controller/CExperiencia.java
+++ src/main/java/com/porfolio/Porfolio/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins="https://bkporfoliotest-be649.web.app/")
+@CrossOrigin(origins="https://bkporfoliotest-be649.web.app")
 
 
 public class CExperiencia {
